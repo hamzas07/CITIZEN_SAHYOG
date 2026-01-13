39d141e0ef37eed9dd1879389ccfa24912eca210
MOHAMMED HAMZA ANSARI <161144800+hamzas07@users.noreply.github.com>
2026-01-13 20:14:13 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://citizen-sahyog.onrender.com"
+  baseURL: "https://citizen-sahyog.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
