4ba526126382953bedac14fbaa0c1393624b5183
Hamza Ansari <hamzaansari26110@gmail.com>
2026-01-14 01:59:41 +0530

    some final fixezzz

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-require("dotenv").config();
 
 const AuthContext = createContext(null);
 
--- frontend/src/services/complaintService.js
+++ frontend/src/services/complaintService.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL + "/api";
 
