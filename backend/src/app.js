3b0efabf022fd826052b1ce6538c313ff0b25b5e
Hamza Ansari <hamzaansari26110@gmail.com>
2026-01-14 02:38:13 +0530

    some final fixezzzzzz

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: ["https://citizen-sahyog-platform.vercel.app", "http://localhost:5173"],
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
